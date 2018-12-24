c7422db8666883a1cfdad70dd0a57b343e63df61
Fiegellan <cfiegel@horizon.csueastbay.edu>
2018-12-24 13:54:27 -0800

    update

--- src/main/python/youtube_mock_interviews/senior_airbnb.py
+++ src/main/python/youtube_mock_interviews/senior_airbnb.py
@@ -9,15 +9,22 @@
 def find_pairs(a,k):
     solution = {}
     for i in a:
+        # if the value is greater than k, then clearly it won't have a solution
         if i < k:
+            # see if the k-i value is in the array
             if k-i in a:
                 # print i, k-i
-                solution[i]=k-i
+                # This checks to see if the k-i term is a dictionary key.
+                # If so pass, else insert.  Removing duplicates
+                if k-i in solution:
+                    pass
+                else:
+                    solution[i]=k-i
 
     return solution
 
 
-array = (1,3,2,5,46,6,74)
+array = (1,3,2,5,46,6,74,4)
 k = 5
 
 print find_pairs(array,k)
