363b0965146a5b429fce8ea1d286577f15b1af85
Fiegellan <cfiegel@horizon.csueastbay.edu>
2018-12-27 13:13:31 -0800

    Update to palindrome

--- src/main/python/Problems/Palindrome_pairs.py
+++ src/main/python/Problems/Palindrome_pairs.py
@@ -1,17 +1,21 @@
-
-
 def palindrome_pairs(list):
     answer = []
-    for i,x in enumerate(list):
-        print x, x[::-1]
+    for i, x in enumerate(list):
+        # print x, x[::-1]
         if x[::-1] in list:
             if i != list.index(x[::-1]):
                 # print 'index 1', i, 'index 2', list.index(x[::-1])
                 a = (i, list.index(x[::-1]))
                 answer.append(a)
 
-    return set((a,b) if a<=b else (b,a) for a,b in answer)
+    a = set((a, b) if a <= b else (b, a) for a, b in answer)
+
+    if len(a) > 0:
+        return a
+    else:
+        return False
 
 
+z = ['ii', ' ', '21']
 
-print palindrome_pairs(['ii','12', '21'])
+print palindrome_pairs(z)
